b048844d82e0a5d4e337fc398b966d932c4b294d
Matt Venables <matt@venabl.es>
2024-09-05 21:45:57 -0400

    Enable cloudflare pages

--- env/server.ts
+++ env/server.ts
@@ -7,6 +7,15 @@ export const env = createEnv({
   experimental__runtimeEnv: process.env,
   extends: [shared],
   server: {
+    /**
+     * Cloudflare Env Vars
+     */
+    CF_PAGES_COMMIT_SHA: z.string().default("unknown"),
+    CF_PAGES_URL: z.string().url().optional(),
+
+    /**
+     *
+     */
     DATABASE_URL: z.string().url(),
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string()
